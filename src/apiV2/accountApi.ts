baba0eb379d0a457352e669679f92a525225a29d
srpiroliro <srpiroliro21@gmail.com>
2025-05-04 13:49:50 +0200

    feat(type): proper types for Account api

--- src/apiV2/accountApi.ts
+++ src/apiV2/accountApi.ts
@@ -1,5 +1,20 @@
-import { makeGetRequest, appendArrayParams, appendQueryParam } from '../utils';
-import { BaseApiV2 } from './baseApiV2';
+import {
+  AccountBalanceChange,
+  AccountDefiActivity,
+  AccountDefiActivityType,
+  AccountDetail,
+  AccountLeaderboard,
+  AccountMetadata,
+  AccountPortfolio,
+  AccountTokenAccount,
+  AccountTransaction,
+  AccountTransfer,
+  AccountType,
+  Flow,
+} from "../types/account";
+import { ApiV2Response, SortByOrder } from "../types/general";
+import { makeGetRequest, appendArrayParams, appendQueryParam } from "../utils";
+import { BaseApiV2 } from "./baseApiV2";
 
 /**
  * AccountAPI class for account-related endpoints
@@ -23,64 +38,72 @@ export class AccountAPI extends BaseApiV2 {
   public async defiActivities(
     address: string,
     options?: {
-      activityType?: string[];
+      activityType?: AccountDefiActivityType[];
       from?: string;
       platform?: string[];
       source?: string[];
       token?: string;
       blockTime?: number[];
       page?: number;
-      pageSize?: number;
-      sortBy?: string;
-      sortOrder?: string;
-    }
-  ): Promise<any> {
+      pageSize?: 10 | 20 | 30 | 40 | 60 | 100;
+      sortBy?: "block_time";
+      sortOrder?: SortByOrder;
+    },
+  ): Promise<ApiV2Response<AccountDefiActivity[]>> {
     let methodUrl = `${this.urlModule}defi/activities?address=${address}`;
 
     if (options) {
-      const { 
-        activityType, from, platform, source, token, 
-        blockTime, page, pageSize, sortBy, sortOrder 
+      const {
+        activityType,
+        from,
+        platform,
+        source,
+        token,
+        blockTime,
+        page,
+        pageSize,
+        sortBy,
+        sortOrder,
       } = options;
 
       if (activityType && Array.isArray(activityType)) {
-        methodUrl = appendArrayParams(methodUrl, 'activity_type', activityType);
+        methodUrl = appendArrayParams(methodUrl, "activity_type", activityType);
       }
 
       if (from) {
-        methodUrl = appendQueryParam(methodUrl, 'from', from);
+        methodUrl = appendQueryParam(methodUrl, "from", from);
       }
 
       if (platform && Array.isArray(platform)) {
-        methodUrl = appendArrayParams(methodUrl, 'platform', platform);
+        methodUrl = appendArrayParams(methodUrl, "platform", platform);
       }
 
       if (source && Array.isArray(source)) {
-        methodUrl = appendArrayParams(methodUrl, 'source', source);
+        methodUrl = appendArrayParams(methodUrl, "source", source);
       }
 
       if (token) {
-        methodUrl = appendQueryParam(methodUrl, 'token', token);
+        methodUrl = appendQueryParam(methodUrl, "token", token);
       }
 
       if (blockTime && Array.isArray(blockTime)) {
-        methodUrl = appendArrayParams(methodUrl, 'block_time', blockTime);
+        methodUrl = appendArrayParams(methodUrl, "block_time", blockTime);
       }
 
-      if (typeof page === 'number') {
-        methodUrl = appendQueryParam(methodUrl, 'page', page);
+      if (typeof page === "number") {
+        methodUrl = appendQueryParam(methodUrl, "page", page);
       }
 
-      if (typeof pageSize === 'number') {
-        methodUrl = appendQueryParam(methodUrl, 'page_size', pageSize);
+      if (typeof pageSize === "number") {
+        methodUrl = appendQueryParam(methodUrl, "page_size", pageSize);
       }
 
       if (sortBy) {
-        methodUrl = appendQueryParam(methodUrl, 'sort_by', sortBy);
+        methodUrl = appendQueryParam(methodUrl, "sort_by", sortBy);
       }
 
       if (sortOrder) {
-        methodUrl = appendQueryParam(methodUrl, 'sort_order', sortOrder);
+        methodUrl = appendQueryParam(methodUrl, "sort_order", sortOrder);
       }
     }
 
@@ -96,80 +119,94 @@ export class AccountAPI extends BaseApiV2 {
   public async transfer(
     address: string,
     options?: {
-      activityType?: string[];
+      activityType?: AccountType[];
       tokenAccount?: string;
       from?: string;
       to?: string;
       token?: string;
       amount?: number[];
       excludeAmountZero?: boolean;
-      flow?: string;
+      flow?: Flow;
       blockTime?: number[];
       page?: number;
-      pageSize?: number;
-      sortBy?: string;
-      sortOrder?: string;
-    }
-  ): Promise<any> {
+      pageSize?: 10 | 20 | 30 | 40 | 60 | 100;
+      sortBy?: "block_time";
+      sortOrder?: SortByOrder;
+    },
+  ): Promise<ApiV2Response<AccountTransfer[]>> {
     let methodUrl = `${this.urlModule}transfer?address=${address}`;
 
     if (options) {
-      const { 
-        activityType, tokenAccount, from, to, token, 
-        amount, excludeAmountZero, flow, blockTime, 
-        page, pageSize, sortBy, sortOrder 
+      const {
+        activityType,
+        tokenAccount,
+        from,
+        to,
+        token,
+        amount,
+        excludeAmountZero,
+        flow,
+        blockTime,
+        page,
+        pageSize,
+        sortBy,
+        sortOrder,
       } = options;
 
       if (activityType && Array.isArray(activityType)) {
-        methodUrl = appendArrayParams(methodUrl, 'activity_type', activityType);
+        methodUrl = appendArrayParams(methodUrl, "activity_type", activityType);
       }
 
       if (tokenAccount) {
-        methodUrl = appendQueryParam(methodUrl, 'token_account', tokenAccount);
+        methodUrl = appendQueryParam(methodUrl, "token_account", tokenAccount);
       }
 
       if (from) {
-        methodUrl = appendQueryParam(methodUrl, 'from', from);
+        methodUrl = appendQueryParam(methodUrl, "from", from);
       }
 
       if (to) {
-        methodUrl = appendQueryParam(methodUrl, 'to', to);
+        methodUrl = appendQueryParam(methodUrl, "to", to);
       }
 
       if (token) {
-        methodUrl = appendQueryParam(methodUrl, 'token', token);
+        methodUrl = appendQueryParam(methodUrl, "token", token);
       }
 
       if (amount && Array.isArray(amount)) {
-        methodUrl = appendArrayParams(methodUrl, 'amount', amount);
+        methodUrl = appendArrayParams(methodUrl, "amount", amount);
       }
 
-      if (typeof excludeAmountZero === 'boolean') {
-        methodUrl = appendQueryParam(methodUrl, 'exclude_amount_zero', excludeAmountZero);
+      if (typeof excludeAmountZero === "boolean") {
+        methodUrl = appendQueryParam(
+          methodUrl,
+          "exclude_amount_zero",
+          excludeAmountZero,
+        );
       }
 
       if (flow) {
-        methodUrl = appendQueryParam(methodUrl, 'flow', flow);
+        methodUrl = appendQueryParam(methodUrl, "flow", flow);
       }
 
       if (blockTime && Array.isArray(blockTime)) {
-        methodUrl = appendArrayParams(methodUrl, 'block_time', blockTime);
+        methodUrl = appendArrayParams(methodUrl, "block_time", blockTime);
       }
 
-      if (typeof page === 'number') {
-        methodUrl = appendQueryParam(methodUrl, 'page', page);
+      if (typeof page === "number") {
+        methodUrl = appendQueryParam(methodUrl, "page", page);
       }
 
-      if (typeof pageSize === 'number') {
-        methodUrl = appendQueryParam(methodUrl, 'page_size', pageSize);
+      if (typeof pageSize === "number") {
+        methodUrl = appendQueryParam(methodUrl, "page_size", pageSize);
       }
 
       if (sortBy) {
-        methodUrl = appendQueryParam(methodUrl, 'sort_by', sortBy);
+        methodUrl = appendQueryParam(methodUrl, "sort_by", sortBy);
       }
 
       if (sortOrder) {
-        methodUrl = appendQueryParam(methodUrl, 'sort_order', sortOrder);
+        methodUrl = appendQueryParam(methodUrl, "sort_order", sortOrder);
       }
     }
 
@@ -185,28 +222,28 @@ export class AccountAPI extends BaseApiV2 {
    */
   public async tokenAccounts(
     address: string,
-    type: string,
+    type: "token" | "nft",
     options?: {
       hideZero?: boolean;
       page?: number;
-      pageSize?: number;
-    }
-  ): Promise<any> {
+      pageSize?: 10 | 20 | 30 | 40;
+    },
+  ): Promise<ApiV2Response<AccountTokenAccount[]>> {
     let methodUrl = `${this.urlModule}token-accounts?address=${address}&type=${type}`;
 
     if (options) {
       const { hideZero, page, pageSize } = options;
 
-      if (typeof hideZero === 'boolean') {
-        methodUrl = appendQueryParam(methodUrl, 'hide_zero', hideZero);
+      if (typeof hideZero === "boolean") {
+        methodUrl = appendQueryParam(methodUrl, "hide_zero", hideZero);
       }
 
-      if (typeof page === 'number') {
-        methodUrl = appendQueryParam(methodUrl, 'page', page);
+      if (typeof page === "number") {
+        methodUrl = appendQueryParam(methodUrl, "page", page);
       }
 
-      if (typeof pageSize === 'number') {
-        methodUrl = appendQueryParam(methodUrl, 'page_size', pageSize);
+      if (typeof pageSize === "number") {
+        methodUrl = appendQueryParam(methodUrl, "page_size", pageSize);
       }
     }
 
@@ -228,53 +265,60 @@ export class AccountAPI extends BaseApiV2 {
       flow?: string;
       blockTime?: number[];
       page?: number;
-      pageSize?: number;
-      sortBy?: string;
-      sortOrder?: string;
-    }
-  ): Promise<any> {
+      pageSize?: 10 | 20 | 30 | 40 | 60 | 100;
+      sortBy?: "block_time";
+      sortOrder?: SortByOrder;
+    },
+  ): Promise<ApiV2Response<AccountBalanceChange[]>> {
     let methodUrl = `${this.urlModule}balance_change?address=${address}`;
 
     if (options) {
-      const { 
-        token, removeSpam, amount, flow, 
-        blockTime, page, pageSize, sortBy, sortOrder 
+      const {
+        token,
+        removeSpam,
+        amount,
+        flow,
+        blockTime,
+        page,
+        pageSize,
+        sortBy,
+        sortOrder,
       } = options;
 
       if (token) {
-        methodUrl = appendQueryParam(methodUrl, 'token', token);
+        methodUrl = appendQueryParam(methodUrl, "token", token);
       }
 
       if (amount && Array.isArray(amount)) {
-        methodUrl = appendArrayParams(methodUrl, 'amount', amount);
+        methodUrl = appendArrayParams(methodUrl, "amount", amount);
       }
 
       if (flow) {
-        methodUrl = appendQueryParam(methodUrl, 'flow', flow);
+        methodUrl = appendQueryParam(methodUrl, "flow", flow);
       }
 
-      if (typeof removeSpam === 'boolean') {
-        methodUrl = appendQueryParam(methodUrl, 'remove_spam', removeSpam);
+      if (typeof removeSpam === "boolean") {
+        methodUrl = appendQueryParam(methodUrl, "remove_spam", removeSpam);
       }
 
       if (blockTime && Array.isArray(blockTime)) {
-        methodUrl = appendArrayParams(methodUrl, 'block_time', blockTime);
+        methodUrl = appendArrayParams(methodUrl, "block_time", blockTime);
       }
 
-      if (typeof page === 'number') {
-        methodUrl = appendQueryParam(methodUrl, 'page', page);
+      if (typeof page === "number") {
+        methodUrl = appendQueryParam(methodUrl, "page", page);
       }
 
-      if (typeof pageSize === 'number') {
-        methodUrl = appendQueryParam(methodUrl, 'page_size', pageSize);
+      if (typeof pageSize === "number") {
+        methodUrl = appendQueryParam(methodUrl, "page_size", pageSize);
       }
 
       if (sortBy) {
-        methodUrl = appendQueryParam(methodUrl, 'sort_by', sortBy);
+        methodUrl = appendQueryParam(methodUrl, "sort_by", sortBy);
       }
 
       if (sortOrder) {
-        methodUrl = appendQueryParam(methodUrl, 'sort_order', sortOrder);
+        methodUrl = appendQueryParam(methodUrl, "sort_order", sortOrder);
       }
     }
 
@@ -291,20 +335,20 @@ export class AccountAPI extends BaseApiV2 {
     address: string,
     options?: {
       before?: string;
-      limit?: number;
-    }
-  ): Promise<any> {
+      limit?: 10 | 20 | 30 | 40;
+    },
+  ): Promise<ApiV2Response<AccountTransaction[]>> {
     let methodUrl = `${this.urlModule}transactions?address=${address}`;
 
     if (options) {
       const { before, limit } = options;
 
       if (before) {
-        methodUrl = appendQueryParam(methodUrl, 'before', before);
+        methodUrl = appendQueryParam(methodUrl, "before", before);
       }
 
-      if (typeof limit === 'number') {
-        methodUrl = appendQueryParam(methodUrl, 'limit', limit);
+      if (typeof limit === "number") {
+        methodUrl = appendQueryParam(methodUrl, "limit", limit);
       }
     }
 
@@ -322,19 +366,19 @@ export class AccountAPI extends BaseApiV2 {
     options?: {
       page?: number;
       pageSize?: number;
-    }
-  ): Promise<any> {
+    },
+  ): Promise<ApiV2Response<any>> {
     let methodUrl = `${this.urlModule}stake?address=${address}`;
 
     if (options) {
       const { page, pageSize } = options;
 
-      if (typeof page === 'number') {
-        methodUrl = appendQueryParam(methodUrl, 'page', page);
+      if (typeof page === "number") {
+        methodUrl = appendQueryParam(methodUrl, "page", page);
       }
 
-      if (typeof pageSize === 'number') {
-        methodUrl = appendQueryParam(methodUrl, 'page_size', pageSize);
+      if (typeof pageSize === "number") {
+        methodUrl = appendQueryParam(methodUrl, "page_size", pageSize);
       }
     }
 
@@ -346,7 +390,7 @@ export class AccountAPI extends BaseApiV2 {
    * @param address Address of an account (required)
    * @returns Promise with account details
    */
-  public async detail(address: string): Promise<any> {
+  public async detail(address: string): Promise<ApiV2Response<AccountDetail>> {
     const methodUrl = `${this.urlModule}detail?address=${address}`;
     return makeGetRequest(methodUrl, this.headers);
   }
@@ -361,8 +405,8 @@ export class AccountAPI extends BaseApiV2 {
   public async rewardsExport(
     address: string,
     timeFrom: number,
-    timeTo: number
-  ): Promise<any> {
+    timeTo: number,
+  ): Promise<ApiV2Response<any>> {
     const methodUrl = `${this.urlModule}reward/export?address=${address}&time_from=${timeFrom}&time_to=${timeTo}`;
     return makeGetRequest(methodUrl, this.headers);
   }
@@ -385,53 +429,88 @@ export class AccountAPI extends BaseApiV2 {
       excludeAmountZero?: boolean;
       flow?: string;
       blockTime?: number[];
-    }
-  ): Promise<any> {
+    },
+  ): Promise<ApiV2Response<any>> {
     let methodUrl = `${this.urlModule}transfer/export?address=${address}`;
 
     if (options) {
-      const { 
-        activityType, tokenAccount, from, to, token, 
-        amount, excludeAmountZero, flow, blockTime
+      const {
+        activityType,
+        tokenAccount,
+        from,
+        to,
+        token,
+        amount,
+        excludeAmountZero,
+        flow,
+        blockTime,
       } = options;
 
       if (activityType && Array.isArray(activityType)) {
-        methodUrl = appendArrayParams(methodUrl, 'activity_type', activityType);
+        methodUrl = appendArrayParams(methodUrl, "activity_type", activityType);
       }
 
       if (tokenAccount) {
-        methodUrl = appendQueryParam(methodUrl, 'token_account', tokenAccount);
+        methodUrl = appendQueryParam(methodUrl, "token_account", tokenAccount);
       }
 
       if (from) {
-        methodUrl = appendQueryParam(methodUrl, 'from', from);
+        methodUrl = appendQueryParam(methodUrl, "from", from);
       }
 
       if (to) {
-        methodUrl = appendQueryParam(methodUrl, 'to', to);
+        methodUrl = appendQueryParam(methodUrl, "to", to);
       }
 
       if (token) {
-        methodUrl = appendQueryParam(methodUrl, 'token', token);
+        methodUrl = appendQueryParam(methodUrl, "token", token);
       }
 
       if (amount && Array.isArray(amount)) {
-        methodUrl = appendArrayParams(methodUrl, 'amount', amount);
+        methodUrl = appendArrayParams(methodUrl, "amount", amount);
       }
 
-      if (typeof excludeAmountZero === 'boolean') {
-        methodUrl = appendQueryParam(methodUrl, 'exclude_amount_zero', excludeAmountZero);
+      if (typeof excludeAmountZero === "boolean") {
+        methodUrl = appendQueryParam(
+          methodUrl,
+          "exclude_amount_zero",
+          excludeAmountZero,
+        );
       }
 
       if (flow) {
-        methodUrl = appendQueryParam(methodUrl, 'flow', flow);
+        methodUrl = appendQueryParam(methodUrl, "flow", flow);
       }
 
       if (blockTime && Array.isArray(blockTime)) {
-        methodUrl = appendArrayParams(methodUrl, 'block_time', blockTime);
+        methodUrl = appendArrayParams(methodUrl, "block_time", blockTime);
       }
     }
 
     return makeGetRequest(methodUrl, this.headers);
   }
-} 
+
+  public async portfolio(
+    address: string,
+  ): Promise<ApiV2Response<AccountPortfolio>> {
+    let methodUrl = `${this.urlModule}portfolio?address=${address}`;
+
+    return makeGetRequest(methodUrl, this.headers);
+  }
+
+  public async metadata(
+    address: string,
+  ): Promise<ApiV2Response<AccountMetadata>> {
+    let methodUrl = `${this.urlModule}metadata?address=${address}`;
+
+    return makeGetRequest(methodUrl, this.headers);
+  }
+
+  public async leaderboard(
+    address: string,
+  ): Promise<ApiV2Response<AccountLeaderboard>> {
+    let methodUrl = `${this.urlModule}leaderboard?address=${address}`;
+
+    return makeGetRequest(methodUrl, this.headers);
+  }
+}
---
+++ src/types/account.ts
@@ -0,0 +1,156 @@
+export enum AccountType {
+  ACTIVITY_SPL_TRANSFER,
+  ACTIVITY_SPL_BURN,
+  ACTIVITY_SPL_MINT,
+  ACTIVITY_SPL_CREATE_ACCOUNT,
+}
+
+export enum AccountDefiActivityType {
+  ACTIVITY_TOKEN_SWAP,
+  ACTIVITY_AGG_TOKEN_SWAP,
+  ACTIVITY_TOKEN_ADD_LIQ,
+  ACTIVITY_TOKEN_REMOVE_LIQ,
+  ACTIVITY_SPL_TOKEN_STAKE,
+  ACTIVITY_SPL_TOKEN_UNSTAKE,
+  ACTIVITY_TOKEN_DEPOSIT_VAULT,
+  ACTIVITY_TOKEN_WITHDRAW_VAULT,
+  ACTIVITY_SPL_INIT_MINT,
+  ACTIVITY_ORDERBOOK_ORDER_PLACE,
+}
+
+export type Flow = "in" | "out";
+
+/* ----------------------- */
+
+export type AccountDetail = {
+  account: string;
+  lamports: number;
+  type: string;
+  executable: boolean;
+  owner_program: string;
+  rent_epoch: number;
+  is_oncurve: number;
+};
+
+export type AccountTransfer = {
+  block_id: number;
+  trans_id: string;
+  block_time: number;
+  time: string;
+  activity_type: string;
+  from_address: string;
+  from_token_account: string;
+  to_address: string;
+  to_token_account: string;
+  token_address: string;
+  token_decimals: number;
+  amount: number;
+  flow: string;
+};
+
+export type AccountDefiActivity = {
+  block_id: number;
+  trans_id: string;
+  block_time: number;
+  time: string;
+  activity_type: string;
+  from_address: string;
+  to_address: string;
+  sources: {
+    platform: string;
+    routers: Array<{
+      token1: string;
+      token1_decimals: number;
+      amount1: string;
+      token2: string;
+      token2_decimals: number;
+      amount2: string;
+    }>;
+    child_routers: Array<{
+      token1: string;
+      token1_decimals: number;
+      amount1: string;
+      token2: string;
+      token2_decimals: number;
+      amount2: string;
+    }>;
+  };
+};
+
+export type AccountBalanceChange = {
+  block_id: number;
+  block_time: number;
+  time: string;
+  trans_id: string;
+  address: string;
+  token_address: string;
+  token_decimals: number;
+  token_account: string;
+  amount: number;
+  pre_balance: number;
+  post_balance: number;
+  change_type: "inc" | "dec";
+  fee: number;
+};
+
+export type AccountTransaction = {
+  slot: number;
+  fee: number;
+  status: "Success" | "Fail";
+  signer: string;
+  block_time: number;
+  tx_hash: string;
+  parsed_instructions: {
+    type: string;
+    program: string;
+    program_id: string;
+  }[];
+  program_ids: string;
+  time: string;
+};
+
+type Token = {
+  token_address?: string; // undefined on native token
+
+  amount: number; // The token raw amount
+  balance: number; // The token balance. Calculated by formula: balance = amount / (10 ^ decimals)
+  token_price: number; // The token price in USD
+  token_decimals: number; // The token decimal
+  token_name: string; // The token name
+  token_symbol: string; // The token symbol
+  token_icon: string; // The token icon
+  value: number; // The token value in USD
+};
+
+export type AccountPortfolio = {
+  total_value: number; // usd
+  native_token: Token;
+  tokens: Token[];
+};
+
+export type AccountTokenAccount = {
+  token_account: string;
+  token_address: string;
+  amount: number;
+  token_decimals: number;
+  owner: string;
+};
+
+export type AccountMetadata = {
+  account_address: string;
+  account_label: string;
+  account_icon: string;
+  account_tags: string[];
+  account_type: string;
+};
+
+export type AccountLeaderboard = {
+  total: number;
+  data: {
+    account: string;
+    sol_values: number;
+    token_values: number;
+    stake_values: number;
+    total_values: number;
+  }[];
+};
